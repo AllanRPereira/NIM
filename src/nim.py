cc1a063eb425229d993889136ccaef382e203f0b
AllanRPereira <allanflyk2@gmail.com>
2017-07-06 12:37:39 -0300

    Modo pessoa-pessoa finalizado!

--- src/nim.py
+++ src/nim.py
@@ -43,8 +43,34 @@ def intelligence(n_toothpicks, min_tooth, max_tooth):
 		return 1
 
 
-def human():
-	pass
+def human(players, n_toothpicks, min_tooth, max_tooth):
+	players[0] = players[0].title()
+	players[1] = players[1].title()
+	player_current = 0
+
+	while True:
+		print("| " * n_toothpicks, end="\n\n")
+		player_remove = int(input(players[player_current] + ", digite quantos palitos deseja remover:"))
+		print("")
+
+		if min_tooth <= player_remove <= max_tooth and player_remove <= n_toothpicks:
+			n_toothpicks -= player_remove
+		elif player_remove > n_toothpicks:
+			print("Número digitado é maior do que o número de palitos disponíveis!!Tente Novamente", end="\n\n")
+			continue
+		else:
+			print("Número digitado fora do intervalo definido!!Tente Novamente", end="\n\n")
+			continue
+			
+		if n_toothpicks <= min_tooth:
+			print("O jogador ", players[player_current], " ganhou a partida!!")
+			return 0
+		else:
+			if player_current == 0:
+				player_current += 1
+			else:
+				player_current -= 1
+
 
 print("Bem Vindo ao NIM!!")
 
@@ -72,15 +98,14 @@ while True:
 	player_or_computer = int(input("Digite 1 para jogar contra o PC ou 2 contra um colega:"))
 	print("")
 
-	if player_or_computer != 1 and 2:
+	if player_or_computer != 1 and player_or_computer != 2:
 		print("Valor para modo de jogo inválido!! Tente Novamente", end="\n\n")
 		time.sleep(2)
 	else:
-		name = input("Digite seu nome:")
+		name_player1 = input("Digite seu nome:")
 		print("")
 		if player_or_computer == 1: 
-			computer(name, n_toothpicks, min_toothpicks, max_toothpicks)
+			computer(name_player1, n_toothpicks, min_toothpicks, max_toothpicks)
 		else:
-			human()
-
-
+			name_player2 = input("Digite o nome do jogador 2:")
+			human([name_player1, name_player2], n_toothpicks, min_toothpicks, max_toothpicks)
